5beeb463d5f7de2973e696e8937b603288e1c5ba
mgageo <mga_geo@yahoo.fr>
2017-01-08 08:16:40 +0100

    version initiale

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # vilaineaval : Bretagne Vivante et Rennes Vilaine Aval
 
-Scripts en environnement Windows 10 : MinGW R
+Scripts en environnement Windows 10 : MinGW R MikTex
 
 Ces scripts exploitent des données en provenance des bases Serena et Biolovision
--- scripts/vilaineaval.sh
+++ scripts/vilaineaval.sh
@@ -103,7 +103,7 @@ EOF
   cat  <<'EOF' > /tmp/README.md
 # vilaineaval : Bretagne Vivante et Rennes Vilaine Aval
 
-Scripts en environnement Windows 10 : MinGW R
+Scripts en environnement Windows 10 : MinGW R MikTex
 
 Ces scripts exploitent des données en provenance des bases Serena et Biolovision
 EOF
